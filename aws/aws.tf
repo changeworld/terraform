ebefa7916a17f9be05ef56d31e9f293d1669937c
Takashi Takebayashi <changesworlds@gmail.com>
2016-08-18 15:49:04 +0900

    Modify Resource Dependencies for AWS

--- aws/aws.tf
+++ aws/aws.tf
@@ -26,3 +26,6 @@ resource "aws_instance" "example" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
 }
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
